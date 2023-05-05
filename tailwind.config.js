bc31c6bc19ea95edc8ef3dd402d91ac15473c20d
PARAS <mehtaparas1901@gmail.com>
2023-05-05 11:26:22 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      xsm: '200px',
+      sm: '640px',
+      md: '768px',
+      mdm: '820px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
